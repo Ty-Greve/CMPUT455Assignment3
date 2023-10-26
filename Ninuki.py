9a85122c25584cc8636a349fbde6f90450b7f4db
ssekowsk <77700607+ssekowsk@users.noreply.github.com>
2023-10-26 16:08:47 -0600

    code from player implementation

--- Ninuki.py
+++ Ninuki.py
@@ -11,7 +11,43 @@ from board_base import DEFAULT_SIZE, GO_POINT, GO_COLOR
 from board import GoBoard
 from board_util import GoBoardUtil
 from engine import GoEngine
+#from game_basics import EMPTY, BLACK, WHITE
 
+class FlatMonteCarloPlayer(object):
+    def __init__(self, numSimulations):
+        self.numSimulations = numSimulations
+
+    def name(self):
+        return "Flat Monte Carlo Player ({0} sim.)".format(self.numSimulations)
+
+    def genmove(self, state): #this should not be state, all instancesneed to be change
+        assert not state.endOfGame()
+        moves = state.legalMoves()
+        numMoves = len(moves)
+        score = [0] * numMoves
+        for i in range(numMoves):
+            move = moves[i]
+            score[i] = self.simulate(state, move)
+        bestIndex = score.index(max(score))
+        best = moves[bestIndex]
+        assert best in state.legalMoves()
+        return best
+
+    def simulate(self, state, move):
+        stats = [0] * 3
+        state.play(move)
+        moveNr = state.moveNumber()
+        for _ in range(self.numSimulations):
+            winner, _ = state.simulate()
+            stats[winner] += 1
+            state.resetToMoveNumber(moveNr)
+        assert sum(stats) == self.numSimulations
+        assert moveNr == state.moveNumber()
+        state.undoMove()
+        eval = (stats[BLACK] + 0.5 * stats[EMPTY]) / self.numSimulations
+        if state.toPlay == WHITE:
+            eval = 1 - eval
+        return eval
 
 class Go0(GoEngine):
     def __init__(self) -> None:
